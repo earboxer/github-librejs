ff2bab9d65790be1e9158b7b79d2faba14223337
Zach DeCook <zdecook@ccel.org>
2020-03-03 13:31:16 -0500

    * Comment Edit: Update for new github change

--- threadreply.user.js
+++ threadreply.user.js
@@ -2,7 +2,7 @@
 // @license magnet:?xt=urn:btih:1f739d935676111cfff4b4693e3816e664797050&dn=gpl-3.0.txt GPL-v3-or-Later
 // @name         thread reply in github
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Fix Comment related functionality on github for librejs/noscript users.
 // @author       Zach DeCook
 // @match        https://github.com/*/*/pull/*
@@ -20,14 +20,15 @@
     });
     // Edit comment!
     jQuery('button.js-comment-edit-button').click(function(e) {
-        this.closest('.js-targetable-comment').classList.add('is-comment-editing');
+        this.closest('.comment').classList.add('is-comment-editing');
     });
     jQuery('button.js-comment-cancel-button').click(function(e) {
-        this.closest('.js-targetable-comment').classList.remove('is-comment-editing');
+        this.closest('.comment').classList.remove('is-comment-editing');
     });
     // hide comment!
     jQuery('button.js-comment-hide-button').click(function(e) {
-        jQuery(this).closest('.edit-comment-hide').find('.js-minimize-comment').removeClass('d-none')
+        jQuery(this).closest('.edit-comment-hide').find('.js-minimize-comment')
+            .removeClass('d-none')
     });
     jQuery('button.js-comment-hide-minimize-form').click(function(e) {
         this.closest('.js-minimize-comment').classList.add('d-none');
