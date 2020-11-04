2959acb23e1cab83c341021657c1ea1f10cbc0d4
Zach DeCook <zdecook@ccel.org>
2020-11-04 11:55:43 -0500

    * Notifications: Create script to redirect blank page back to notifications

---
+++ notifications.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name        Notifications - github.com
+// @namespace   Violentmonkey Scripts
+// @match       https://github.com/notifications/beta/archive
+// @grant       none
+// @version     1.0
+// @author      Zach DeCook
+// @description 11/4/2020, 11:47:40 AM
+// ==/UserScript==
+window.location="/notifications";
